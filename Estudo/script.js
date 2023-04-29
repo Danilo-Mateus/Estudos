6aba585076d33fab37baae9676af057a8a283997
Danilo-Mateus <danilomateus739@gmail.com>
2023-04-29 15:06:31 -0300

    atualização

--- Estudo/script.js
+++ Estudo/script.js
@@ -1,1 +1,20 @@
-let
+const calcu = {
+    soma: function(a, b){
+    return a + b;
+
+    },
+    subtrair: function(a, b){
+    return a - b;
+    } ,
+    multiplicar: function(a, b){
+    return a * b;
+
+    },
+    divisao: function(a, b){
+    return a / b;
+    }
+}
+console.log(calcu.soma(3,4))
+console.log(calcu.subtrair(3,4))
+console.log(calcu.multiplicar(3,4))
+console.log(calcu.divisao(3,4))
